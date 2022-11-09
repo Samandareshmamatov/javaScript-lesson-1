483fb41291f6f8fad1ec71ba06a6f0c97b64e563
Samandareshmamatov <samandareshmamatov1127@gmail.com>
2022-11-09 20:50:05 +0500

    Add this project

---
+++ main.js
@@ -0,0 +1,27 @@
+const firstName = prompt("Ismingizni kiriting : "); // CamelCase
+let age = prompt("Yoshingizni kiriting : ");
+const adress = prompt("Manzilingizni kiriting : ");
+let tel = prompt("Telefon raqamingizni kiriting : ");
+
+document.write("Sizning ismingiz : " + firstName + `<br>`);
+document.write("Sizning yoshingiz : " + age + `<br>`);
+document.write("Sizning raqamingiz : " + tel + `<br>`);
+document.write(firstName + " siz " + adress + "da yashaysiz");
+document.write("Consoleda Archa bor 😂😎🍀");
+
+console.log(
+    `
+           ⭐ 
+          🍀🍀 
+         🍀🍀🍀 
+        🍀🎉🍀🍀 
+       🍀🎇🍀🍀🍀 
+      🍀🍀🍀🎇🍀🍀
+     🍀🍀🍀🍀🍀🍀🍀
+    🍀🎇🍀🍀🎉🍀🍀🍀 
+   🍀🍀🍀🍀🍀🍀🍀🍀🍀
+  🍀🎉🍀🍀🍀🎇🍀🍀🍀🍀
+🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀
+          🪵🪵🪵
+          🪵🪵🪵
+         `)
